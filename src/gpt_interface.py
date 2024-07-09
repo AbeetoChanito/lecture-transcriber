b71de75ab4ccf0e908f948b06bd0215963fa094c
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2024-07-09 12:06:44 -0400

    add in gpt interface

--- src/gpt_interface.py
+++ src/gpt_interface.py
@@ -1,19 +1,17 @@
 from gpt4all import GPT4All
 
-class GPTInterface:
-    def __init__(self, model: str = "orca-mini-3b-gguf2-q4_0.gguf"):
-        self.model = GPT4All(model)
+model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
         
-    def get_gpt_output(self, lecture_transcribed: str, audio_transcribed: str, max_tokens=1024):
-        with self.model.chat_session():
-            output = f"""
-            You have both the text from a lecture's slideshow and its audio transcription. Please summarize the lecture.
+def get_gpt_output(lecture_transcribed: str, audio_transcribed: str, max_tokens : int = 1024):
+    with model.chat_session():
+        output = f"""
+        You have both the text from a lecture's slideshow and its audio transcription. Please summarize the lecture.
 
-            Lecture Transcribed:
-            {lecture_transcribed}
+        Lecture Transcribed:
+        {lecture_transcribed}
 
-            Audio Transcribed:
-            {audio_transcribed}
-            """
+        Audio Transcribed:
+        {audio_transcribed}
+        """
 
-            return self.model.generate(prompt=output, max_tokens=min(max_tokens, 1024))
+        return model.generate(prompt=output, max_tokens=min(max_tokens, 1024))
