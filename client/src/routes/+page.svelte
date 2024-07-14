6c76f4f7ed1d6b628b2d2cf14754e169eca37e5e
AbeetoChanito <132693471+AbeetoChanito@users.noreply.github.com>
2024-07-14 11:20:13 -0400

    add core frontend functionality

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 let files: FileList | null = null;
+let videoUrl: string | null = null;
 let uploadError: any = null;
 let videoTranscribed: string | null = null;
+let submitted: boolean = false;
+let output: string | null = null;
 
 async function uploadFile(formData: FormData): Promise<any> {
     console.log("Fetching");
@@ -21,25 +24,37 @@ async function uploadFile(formData: FormData): Promise<any> {
     return await response.json();
 }
 
+async function submit(): Promise<void> {
+    submitted = true;
+
+    output = "Processing...";
+
+    let file = files?.[0] as File;
+
+    let formData = new FormData();
+    formData.append("file", file);
+
+    try {
+        const response = await uploadFile(formData);
+        videoTranscribed = response["video_transcribed"];
+        output = `Video transcribed:\n${videoTranscribed}`;
+    } catch (error) {
+        uploadError = error;
+        output = `Error uploading file:\n${uploadError}`;
+    }
+
+    console.log(output);
+
+    files = null;
+}
+
 $: if (files) {
     (async () => {
         console.log("File updated");
 
-        let file: File = files[0];
-
-        let formData = new FormData();
-        formData.append("file", file);
-
-        try {
-            const response = await uploadFile(formData);
-            videoTranscribed = response["video_transcribed"];
-            console.log(`Video transcribed: ${videoTranscribed}`);
-        } catch (error) {
-            uploadError = error;
-            console.log(`Error uploading file: ${uploadError}`);
-        }
-
-        files = null;
+        let file = files[0];
+        
+        videoUrl = URL.createObjectURL(file);
     })();
 }
 </script>
@@ -50,8 +65,8 @@ $: if (files) {
 
 <style>
     body {
-        margin: 0;
         font-family: lexend;
+        margin: 0;
     }
 
     .nav {
@@ -64,35 +79,89 @@ $: if (files) {
 
     .nav a {
         color: #f2f2f2;
-        text-align: center;
         padding: 14px 16px;
         text-decoration: none;
         font-size: 50px;
+        left: 10px;
     }
 
     .nav .credit {
-        margin-left: auto;
         color: #f2f2f2;
         font-size: 20px;
-        font-style: italic;
         padding: 25px;
+        text-decoration: underline;
+    }
+
+    .upload-video {
+        width: 500px;
+        height: auto;
+        padding: 25px;
+        margin: 10px 0;
+        border: 2px solid #ccc;
+        box-sizing: border-box;
+        position: relative;
+        left: 10px;
+    }
+
+    .upload-video label {
+        font-size: 20px;
+    }
+
+    .upload-video input {
+        font-size: 20px;
+    }
+
+    .upload-video button {
+        font-size: 20px;
+    }
+
+    .video-container video {
+        margin-left: 10px;
+        width: 50%;
+        height: 50%;
+    }
+
+    .output {
+        width: 1000px;
+        height: auto;
+        padding: 10px;
+        margin: 10px 0;
+        border: 2px solid #ccc;
+        box-sizing: border-box;
+        position: relative;
+        left: 10px;
+        font-size: 10px;
     }
 </style>
 
 <body>
+
 <div class="nav">
     <a href="/">Video Transcriber</a>
     <a href="https://github.com/AbeetoChanito" class="credit" target="_blank">made by Abinav Kumar</a>
 </div>
-</body>
 
+<div class="upload-video">
 <label for="video">Upload a video:</label>
 <input accept="video/mp4" bind:files id="video" name="video" type="file"/>
-
-{#if uploadError}
-    <p>Error uploading file! {uploadError}</p>
+<br>
+{#if files}
+<button on:click={submit} style={submitted ? 'display: none' : ''}>Submit Video</button>
 {/if}
+</div>
 
-{#if videoTranscribed}
-    <p>Video transcribed: {videoTranscribed}</p>
+<div class="video-container">
+{#if videoUrl}
+<video controls>
+    <source src={videoUrl} />
+    <track kind="captions" />
+</video>
 {/if}
+</div>
+
+{#if output} 
+    <div class="output">
+    <h1>{output}</h1>
+    </div>
+{/if}
+</body>
